6b880018371d27b4c13a0e5a98e3166637e8be9a
Olga7 test <7enovaoa@gmail.com>
2024-11-08 23:51:19 +0000

    Modified version using pool, Callable, Future

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,54 @@
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        List<Thread> threads = new ArrayList<>();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        final ExecutorService threadPool = Executors.newFixedThreadPool(25);
+        List<Future<Integer>> futures = new ArrayList<>();
 
         String[] texts = new String[25];
-
         for (int i = 0; i < texts.length; i++) {
             final int index = i;// Зафиксируем значение индекса в отдельной переменной
-            Runnable logic = () -> {
+            Callable<Integer> logic = () -> {
                 texts[index] = generateText("aab", 30_000); // используем переменную с зафиксированным индексом
+                return findMaxInterval(texts[index]);
             };
-            Thread thread = new Thread(logic);
-            threads.add(thread);
-            thread.start();
-
+            futures.add(threadPool.submit(logic)); // отправляем задачу в пул потоков и добавляем Future в список
         }
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
-        }
+        long startTs = System.currentTimeMillis();// start time
+        threadPool.shutdown();
 
-        long startTs = System.currentTimeMillis(); // start time
-        for (String text : texts) {
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
-                        }
-                    }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
-                }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+        int maxInterval = 0; // переменная максимального значения размеров интервалов
+        for (Future future : futures) {
+            int result = (int) future.get();
+            maxInterval = Math.max(maxInterval, result);
         }
         long endTs = System.currentTimeMillis(); // end time
-
         System.out.println("Time: " + (endTs - startTs) + "ms");
+        System.out.println("Max interval: " + maxInterval);
+    }
+
+    private static Integer findMaxInterval(String text) {
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (i >= j) {
+                    continue;
+                }
+                boolean bFound = false;
+                for (int k = i; k < j; k++) {
+                    if (text.charAt(k) == 'b') {
+                        bFound = true;
+                        break;
+                    }
+                }
+                if (!bFound && maxSize < j - i) {
+                    maxSize = j - i;
+                }
+            }
+        }
+        return maxSize;
     }
 
     public static String generateText(String letters, int length) {
